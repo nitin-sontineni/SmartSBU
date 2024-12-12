fb5361c2be2c4d175ae37adb2fe043a37b2413e7
Nitin Gopala Krishna Sontineni <nitinsontineni@gmail.com>
2024-12-12 18:22:27 -0500

    Update firebaseConfig.jsx
    Removed Google Key as project made public for access to professor to evaluate the project

--- frontend/src/components/firebaseConfig.jsx
+++ frontend/src/components/firebaseConfig.jsx
@@ -3,15 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your Firebase config from Firebase Console
-const firebaseConfig = {
-    apiKey: "AIzaSyBrp11jB23ouVyS9P8DgTTgg6wbqfbHMqk",
-    authDomain: "smartsbu-95217.firebaseapp.com",
-    projectId: "smartsbu-95217",
-    storageBucket: "smartsbu-95217.firebasestorage.app",
-    messagingSenderId: "356374619380",
-    appId: "1:356374619380:web:27fe411faa71d1f8a96aa1",
-    measurementId: "G-P3FZ0PMRMV"
-  };
+const firebaseConfig = "paste your Firebase confi here"
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
